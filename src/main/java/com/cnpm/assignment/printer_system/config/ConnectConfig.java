893c531bf3dd819b3b08babb87eb97e97fb54998
daoduyquy321323 <daoduyquylop97@gmail.com>
2024-12-02 00:08:26 +0700

    deploy

--- src/main/java/com/cnpm/assignment/printer_system/config/ConnectConfig.java
+++ src/main/java/com/cnpm/assignment/printer_system/config/ConnectConfig.java
@@ -10,7 +10,7 @@ public class ConnectConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
-                .allowedOrigins("http://localhost:3000") // Chỉ định domain được phép truy cập
+                .allowedOrigins("https://print-system-fe-production.up.railway.app") // Chỉ định domain được phép truy cập
                                                          // (thay bằng domain của
                 // bạn)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Chỉ định các phương thức được phép
